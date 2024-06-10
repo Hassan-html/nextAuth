b63a10d4ee9d9b8f4a883dc331988dfdadc4a444
HSN-CODES <logics.nook@gmail.com>
2024-06-10 11:42:00 +0500

    "trying to fix vercel problems"

--- middleware.ts
+++ middleware.ts
@@ -5,23 +5,23 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const url = request.url;
   const token = await getToken({ req: request });
-  const auth = token?.Role;
-  // console.log(token, auth);
+  const roleAuth = token?.Role;
+  // console.log(token, roleAuth);
   if (token && url.includes("/account")) {
     if (
-      auth === "teacher" ||
+      roleAuth === "teacher" ||
       url.includes("/staff") ||
       url.includes("/adminPages")
     ) {
       return NextResponse.redirect(new URL("/pages/Teacher", request.url));
     } else if (
-      auth === "staff" ||
+      roleAuth === "staff" ||
       url.includes("/Teacher") ||
       url.includes("/adminPages")
     ) {
       return NextResponse.redirect(new URL("/pages/staff", request.url));
     } else if (
-      auth === "adminAccount" ||
+      roleAuth === "adminAccount" ||
       url.includes("/staff") ||
       url.includes("/Teacher")
     ) {
